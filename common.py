8586c300016fac4bd725b6bff5aa219a354f773b
spencer <spanglies@users.noreply.github.com>
2024-03-02 17:10:14 -0700

    changed Python scripts to have all the S3 code in one file.

---
+++ common.py
@@ -0,0 +1,42 @@
+import boto3
+import os
+import io
+
+
+def connect_s3():
+    s3_token = os.getenv("CLOUDFLARE_S3_token_id")
+    s3_secret = os.getenv("cloudflare_s3_token_secret")
+    account_id = os.getenv("CLOUDFLARE_ACCOUNT_ID")
+    bucket_id = os.getenv("s3_bucket_id")
+    s3 = boto3.resource(service_name="s3",
+                        endpoint_url=f"https://{account_id}.r2.cloudflarestorage.com/",
+                        aws_access_key_id=s3_token,
+                        aws_secret_access_key=s3_secret
+                        )
+    return bucket_id, s3
+
+
+def upload_s3(from_dir=None, to_dir=None):
+    if from_dir is None:
+        from_dir = "public/zips"
+    if to_dir is None:
+        to_dir = "zips"
+
+    bucket_id, s3 = connect_s3()
+
+    for item in os.listdir(from_dir):
+        with io.FileIO(f"{from_dir}/{item}") as file:
+            s3.ObjectSummary(bucket_name=bucket_id, key=f'{to_dir}/{item}').put(Body=file)
+
+
+def download_static_refs():
+    bucket_id, s3 = connect_s3()
+    for item in s3.Bucket(name=bucket_id).objects.all():
+        if not f"{item.key}".startswith("content"):
+            continue
+        path = f"{item.key}"
+        s3_file = item.get()["Body"]
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with io.FileIO(path, "w") as local_file:
+            for i in s3_file:
+                local_file.write(i)
